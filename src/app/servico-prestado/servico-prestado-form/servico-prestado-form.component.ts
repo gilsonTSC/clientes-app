9f9281cd60d1fef7706bfe4a761e61656734f464
Gilson <gilson.costa@dce.ufpb.br>
2020-06-20 14:53:52 -0300

    Salvando o serviço prestado

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../clientes/cliente';
 import { ClientesService } from '../../clientes.service'
 import { ServicoPrestado } from '../servicoPrestado';
+import { ServicoPrestadoService } from '../../servico-prestado.service'
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -12,9 +13,12 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
   servico: ServicoPrestado;
+  success: boolean = false;
+  errors: String[];
 
   constructor(
-    private clientesService: ClientesService
+    private clientesService: ClientesService,
+    private servicoPrestadoService: ServicoPrestadoService
   ) {
     this.servico = new ServicoPrestado();
   }
@@ -25,7 +29,16 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.servico);
+    this.servicoPrestadoService
+        .salvar(this.servico)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+          this.servico = new ServicoPrestado();
+        }, errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        });
   }
 
 }
