ef0015d97c9676f08ea57bb80ddb43c9b34bd18b
Gilson <gilson.costa@dce.ufpb.br>
2020-06-23 19:33:14 -0300

    Logando e obtendo o token com o formulário de login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Usuario } from './login/usuario';
 import { Observable } from 'rxjs';
 
@@ -11,6 +11,9 @@ import { environment } from '../environments/environment';
 export class AuthService {
 
   apiURL: string = environment.apiURLBase + "/api/usuarios";
+  tokenURL: string = environment.apiURLBase + environment.obterTokenUrl;
+  clientId: string = environment.clientId;
+  clientSecret: string = environment.clientSecret;
 
   constructor(
     private http: HttpClient
@@ -18,6 +21,18 @@ export class AuthService {
 
   salvar(usuario: Usuario) : Observable<any> {
     return this.http.post<any>(this.apiURL, usuario);
+  }
+
+  tentarLogar(username: string, password: string) : Observable<any> {
+    const params = new HttpParams()
+                      .set('username', username)
+                      .set('password', password)
+                      .set('grant_type', 'password');
+    const headers = {
+      'Authorization': 'Basic ' + btoa(`${this.clientId}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    return this.http.post(this.tokenURL, params.toString(), { headers });
   }
 
 }
