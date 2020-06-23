63f9c5ed84a8422f7da6824cf008298c44f033cf
Gilson <gilson.costa@dce.ufpb.br>
2020-06-23 19:06:59 -0300

    Refatorando o formulario de login/cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +12,13 @@ export class LoginComponent {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
+  errors: string[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   onSubmit(){
@@ -31,7 +35,22 @@ export class LoginComponent {
   }
 
   cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
 
+    this.authService
+        .salvar(usuario)
+        .subscribe( response => {
+          this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+          this.cadastrando = false;
+          this.username = "";
+          this.password = "";
+          this.errors = null;
+        }, errorResponse => {
+          this.mensagemSucesso = null;
+          this.errors = errorResponse.error.errors;
+        });
   }
 
 }
